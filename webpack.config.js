f07d50415ea1a8a8be7c5d0c7cf7671f72cc4f3c
Vadim-Galimov <andrvg1994@gmail.com>
2023-07-16 17:34:29 +0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
 
  },
  plugins: [new HtmlWebpackPlugin({
-   template: path.resolve(__dirname, 'src', 'index.html'),
-   scriptLoading: "blocking"
+   template: path.resolve(__dirname, 'src', 'index.html')
  })],
  module: {
    rules: [
